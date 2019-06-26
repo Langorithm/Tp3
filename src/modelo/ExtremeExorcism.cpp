c8452651e3097a620d4b1d1f82d80c0002cda45e
QuanticSausage <jonatan.dan@hotmail.com>
2019-06-26 19:30:23 -0300

    empeze a pasar ejecutar accion

--- src/modelo/ExtremeExorcism.cpp
+++ src/modelo/ExtremeExorcism.cpp
@@ -6,7 +6,9 @@ ExtremeExorcism::ExtremeExorcism(Habitacion h, set<Jugador> jugadores, PosYDir f
     // Me hago el fantasma principal y lo meto en this
     //fantasma inicial = hagoFantasma(acciones_fantasma, f_init);
     for (auto j : jugadores){
-        // insertar el trie jugadores
+        // insertar en el trie jugadores
+        // insertar en jvPriv y jvPub
+        // uso _revivirTodosLosJugadores
     }
     // Usando contexto defino las posiciones y direcciones de los jugadores
 };
@@ -18,7 +20,21 @@ void ExtremeExorcism::pasar(){
 
 
 void ExtremeExorcism::ejecutarAccion(Jugador j, Accion a){
-    // COMPLETAR
+    infoJugadorPriv jPriv = _jugadores.at(j);
+    Evento evento_anterior = jPriv.acciones.back();
+
+    if (a == DISPARAR){ // esDisparo(a)
+        Evento evento_disparar = evento_anterior;
+        evento_disparar.dispara = true;
+        jPriv.acciones.push_back(evento_disparar);
+
+            // if _matar_fantasmas(evento_disparar.pos, evento_disparar.pos) then nuevaROnda
+    } else if( a != DISPARAR && a != ESPERAR ){ // esMover
+        Pos nuevaPosicion = evento_anterior.pos;
+        if(){
+
+        }
+    }
 };
 
 
